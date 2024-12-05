e4aefda3857a4addd3c9fd0d8f8f43bff426be84
davidetoscano129 <toscano.davidew@gmail.com>
2024-12-05 17:39:07 +0100

    feat: wip

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express'); // Importo express
 const post = require('./data'); // Importo il router definito in post.js
 const app = express(); // Creo un'istanza dell'app express
 const port = 3000;
+const postsRouter = require("./routers/posts");
 
 app.use(express.static("public")); // Configura express per servire file statici (es. immagini) dalla cartella public
+
+app.use("/posts", postsRouter);
 
 // Rotta di default
 app.get('/', (req, res) => {
